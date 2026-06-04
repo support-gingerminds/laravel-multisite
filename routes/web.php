2bbd2bfe998df7cddae5914ce591ee5e980b3d8b
tlenoble <tlenoble@gingerminds.fr>
2026-06-04 10:03:44 +0200

    fix(Admin) add admin prefix

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Gingerminds\LaravelMultisite\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('web')
+    ->prefix(config('gingerminds-core.admin_prefix'))
     ->name('gingerminds-multisite.')
     ->group(function () {
         Route::resource('sites', SiteController::class);
